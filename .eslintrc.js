1fc443c7c41301975bdd4b1bf1c23b239f2d0848
gekronik <sergeybv22@gmail.com>
2023-12-21 02:31:00 +0300

    add max-len rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,10 +26,13 @@ module.exports = {
         'react/jsx-indent': [2, 4], // indentMode отступ 4 пробела
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'react/jsx-filename-extension': [
+            2,
+            { extensions: ['.js', '.jsx', '.tsx'] },
+        ],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
-        'max-len': 'off',
+        'max-len': ['error', { ignoreComments: true }],
         'no-unused-vars': 'warn',
         'react/require-default-props': 'off',
         'react/react-in-jsx-scope': 'off',
