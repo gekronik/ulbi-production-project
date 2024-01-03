98c77bfef90e676e585807fa89d674f24623c6be
gekronik <sergeybv22@gmail.com>
2024-01-04 02:34:39 +0300

    add redux store, add first entity

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { render } from 'react-dom';
-import './app/styles/index.scss';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
@@ -8,12 +8,14 @@ import './shared/config/i18n/i18n';
 import 'app/styles/index.scss';
 
 render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>,
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>,
     document.getElementById('root'),
 );
