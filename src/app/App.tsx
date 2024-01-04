dcbf082021ad9465b793a06ba5a3ae09a4784bda
gekronik <sergeybv22@gmail.com>
2024-01-04 18:45:58 +0300

    fix dark theme color font

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import React, { Suspense, useState } from 'react';
+import React, { Suspense } from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Modal } from 'shared/ui/Modal/Modal';
 
 function App() {
     const { theme } = useTheme();
