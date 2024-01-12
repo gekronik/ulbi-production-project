6080a5d76580ad242e30e8b22e885e3d14796bd2
gekronik <sergeybv22@gmail.com>
2024-01-13 02:11:36 +0300

    add instance api

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,29 +1,30 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 import { User, userActions } from 'entities/User';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
+import { ThunkConfig } from 'app/providers/StoreProvider';
 
 interface LoginByUsernameProps {
     username: string
     password: string
 }
 
-export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, {rejectValue: string}>( // 1 раз диспатч отрабатывает
+export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, ThunkConfig<string>>( // 1 раз диспатч отрабатывает
     'login/loginByUsername',
-    async (authData, thunkAPI) => {
+    async (authData, thunkApi) => {
+        const { extra, dispatch, rejectWithValue } = thunkApi;
+
         try {
-            const response = await axios.post<User>('http://localhost:8000/login', authData);
+            const response = await extra.api.post<User>('/login', authData);
 
             if (!response.data) {
                 throw new Error();
             }
             localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
-            thunkAPI.dispatch(userActions.setAuthData(response.data)); // 2 раз диспатч отрабатывает
-
+            dispatch(userActions.setAuthData(response.data)); // 2 раз диспатч отрабатывает
             return response.data; // 3 раз диспатч отрабатывает
         } catch (e) {
             console.log(e);
-            return thunkAPI.rejectWithValue('error');
+            return rejectWithValue('error');
         }
     },
 );
---
+++ src/shared/api/api.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
+
+export const $api = axios.create({
+    baseURL: __API__,
+    headers: { // требуется для получения каких либо данных авторизованным пользоветлям
+        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY), // проверяется наличие самого заголовка, имитция авторизации
+    },
+});
