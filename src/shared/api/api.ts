04e7d1c830a640e2b760e6d9df85a7248a23a8d7
gekronik <sergeybv22@gmail.com>
2024-03-15 22:52:21 +0300

    fix

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,7 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: { // требуется для получения каких либо данных авторизованным пользоветлям
+    headers: { // требуется для получения каких либо данных авторизованным пользоветлямv
         authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '', // проверяется наличие самого заголовка, имитция авторизации
     },
 });
