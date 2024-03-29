875a6c6b0a16c5c60414878b39ef838eca726315
gekronik <sergeybv22@gmail.com>
2024-03-29 20:43:19 +0300

    add comment list

---
+++ src/entities/Comment/ui/CommentList/CommentList.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { CommentList } from './CommentList';
+
+export default {
+    title: 'shared/CommentList',
+    component: CommentList,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof CommentList>;
+
+const Template: ComponentStory<typeof CommentList> = (args) => <CommentList {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
--- src/pages/ArticleDetailsPage/index.ts
+++ src/pages/ArticleDetailsPage/index.ts
@@ -1,3 +1,5 @@
 export {
     ArticleDetailsPageAsync as ArticleDetailsPage,
 } from './ui/ArticleDetailsPage/ArticleDetailsPageAsync';
+
+export { ArticleDetailsCommentsSchema } from './model/types/ArticleDetailsCommentsSchema';
