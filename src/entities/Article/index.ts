1a56e68ccc86e6fe73b3cda874e736dc20201d1d
gekronik <sergeybv22@gmail.com>
2024-04-05 22:20:56 +0300

    add fetching articles, view type selector

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -8,3 +8,5 @@ export type { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
 export {
     ArticleList,
 } from './ui/ArticleList/ArticleList';
+
+export { ArticleViewSelector } from './ui/ArticleViewSelector/ArticleViewSelector';
---
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -0,0 +1,51 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import ListIcon from 'shared/assets/icons/list.svg';
+import TileIcon from 'shared/assets/icons/tile.svg';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Icon } from 'shared/ui/Icon/Icon';
+import cls from './ArticleViewSelector.module.scss';
+import { ArticleView } from '../../model/types/article';
+
+interface ArticleViewSelectorProps {
+    className?: string
+    view: ArticleView
+    onViewClick?: (view: ArticleView) => void
+}
+
+const viewTypes = [
+    {
+        view: ArticleView.TILE,
+        icon: TileIcon,
+    },
+    {
+        view: ArticleView.LIST,
+        icon: ListIcon,
+    },
+];
+
+export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
+    const { className, view, onViewClick } = props;
+
+    // 55 - 16:10
+    const onClick = (newView: ArticleView) => () => {
+        onViewClick?.(newView);
+    };
+
+    return (
+        <div className={classNames(cls.articleViewSelector, {}, [className])}>
+            {viewTypes.map((viewType) => (
+                <Button
+                    theme={ButtonTheme.CLEAR}
+                    onClick={onClick(viewType.view)}
+
+                >
+                    <Icon
+                        Svg={viewType.icon}
+                        className={classNames('', { [cls.notSelected]: viewType.view !== view })}
+                    />
+                </Button>
+            ))}
+        </div>
+    );
+});
